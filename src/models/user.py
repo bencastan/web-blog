376a4150250147521ffeda140e00d7a6ba5a9c33
Ben Castan <ben@castanfamily.com>
2018-04-02 10:06:47 +1000

    web_blog Chapter 53 Added some of the User class but the handling of errors and such is a bit scratchy Also thinking that password encryption is not handled. Make sure to encrypt at the web page layer which means no clear text password handled

--- src/models/user.py
+++ src/models/user.py
@@ -1,24 +1,54 @@
+from src.common.database import Database
 
 
 class User(object):
-    def __init__(self):
-        pass
+    def __init__(self, email, password, _id=None):
+        self.email = email
+        self.password = password
+        self._id = uuid.uuid4().hex if _id in None else _id
 
-    def get_by_email(self):
-        pass
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one("users", {"email": email})
+        if data is not None:
+            return cls(**data)
+        return None
 
-    def get_by_id(self):
-        pass
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one("users", {"email": _id})
+        if data is not None:
+            return cls(**data)
 
-    def login_valid(self):
-        # Check whether a user's email is matches the password they sent us.
-        pass
+    @staticmethod
+    def login_valid(email, password):
+        # Check whether a user's email matches the password they sent us.
+        user = User.get_by_email(email)
+        if user is not None:
+            # check the password
+            return user.password == password
+        return False
 
-    def register(self):
-        pass
+    @classmethod
+    def register(cls, email, password):
+        user = cls.get_by_email(email)
+        if user is None:
+            # User does not exist so need to create it
+            new_user = cls(email, password)
+            new_user.save_to_mongo()
+            return True
+        else:
+            # The user exist, so no need to register the user
+            return False
 
     def login(self):
         pass
 
     def get_blogs(self):
+        pass
+
+    def json(self):
+        pass
+
+    def save_to_mongo(self):
         pass
