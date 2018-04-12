66092bc9effb4aa72e5fd2642422d6399b312996
Ben Castan <ben@castanfamily.com>
2018-04-12 17:45:59 +1000

    web_blog Section 4, Lecture 62 Almost ready for Bootstrap

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ app.secret_key = "BENC"
 
 
 @app.route('/')
-def hello_world():
-    return render_template('login.html')
+def hello_template():
+    return render_template('home.html')
 
 
 @app.before_first_request
@@ -17,12 +17,32 @@ def initialize_database():
     Database.initialize()
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login') # www.mysite.com./api/register
+def login_template():
+    return render_template('login.html')
+
+
+@app.route('/register') # www/mysite.com/api/register
+def register_template():
+    return render_template('register.html')
+
+
+@app.route('/auth/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
     if User.login_valid(email, password):
         User.login(email)
+
+    return render_template("profile.html", email=session['email'])
+
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
 
     return render_template("profile.html", email=session['email'])
 
